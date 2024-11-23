74a58c96f23de57f67add22aa16c199051a508bb
Prasenjit <prasenjit.aluni@gmail.com>
2024-11-23 12:55:02 +0530

    Commit

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body, ValidationPipe, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, ValidationPipe, Param, UseGuards } from '@nestjs/common';
 import { PlayerService } from './player.service';
 import { CreatePlayerDto } from './dto/create.player.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('player')
 export class PlayerController {
@@ -8,18 +9,21 @@ export class PlayerController {
     constructor(private readonly playerService:PlayerService){}
 
     @Post()
+    @UseGuards(AuthGuard)
     create(@Body(new ValidationPipe()) createPlayerDto: CreatePlayerDto) {
         return this.playerService.create(createPlayerDto);
     }
 
     @Get()
     findAll(){
+        var str = Math.round(100*Math.random());
+        console.log('-: Player List :- '+str+'\n')
         return this.playerService.findAll();
     }
 
     @Get(':id')
     findOne(@Param('id') id: number) {
-        console.log('++++', id, typeof(+id));
+        console.log('-: Player Details ', id, typeof(+id));
         return this.playerService.findOne(+id);
     }
 }
--- src/root.module.ts
+++ src/root.module.ts
@@ -2,22 +2,33 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlayerModule } from './player/player.module';
 import { AuthModule } from './auth/auth.module';
+import { Player } from './player/entity/player.entity';
 
 @Module({
   imports: [
     PlayerModule,
     AuthModule,
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'ep-withered-frost-a5etb539.us-east-2.aws.neon.tech',
-      ssl: true,
-      port: 5432,
-      database:'nestcrud',
-      username: 'neondb_owner',
-      password: 'ysY6GO7HKcBl',
-      entities: [__dirname+'/**/*.entity{.ts,.js}'],
-      synchronize: true
-    })
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestjs',
+      entities: [Player],//[__dirname+'/**/*.entity{.ts,.js}']
+      synchronize: true,
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'ep-withered-frost-a5etb539.us-east-2.aws.neon.tech',
+    //   ssl: true,
+    //   port: 5432,
+    //   database:'nestcrud',
+    //   username: 'neondb_owner',
+    //   password: 'ysY6GO7HKcBl',
+    //   entities: [__dirname+'/**/*.entity{.ts,.js}'],
+    //   synchronize: true
+    // })
   ],
   controllers: [],
   providers: [],
