7afa26707570b35c57a4086fd5caed12b960234f
Prasenjit <prasenjit.aluni@gmail.com>
2024-11-25 15:16:08 +0530

    COmmit

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Post, Body, ValidationPipe, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, ValidationPipe, Param, UseGuards, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { PlayerService } from './player.service';
 import { CreatePlayerDto } from './dto/create.player.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer'
+import { extname } from 'path'
 
 @Controller('player')
-@UseGuards(AuthGuard)
+//@UseGuards(AuthGuard)
 export class PlayerController {
 
     constructor(private readonly playerService:PlayerService){}
@@ -15,6 +18,24 @@ export class PlayerController {
         return this.playerService.create(createPlayerDto);
     }
 
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+          destination: './uploads'
+          , filename: (req, file, cb) => {
+            // Generating a 32 random chars long string
+            const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+            //Calling the callback passing the random name generated with the original extension name
+            cb(null, `${randomName}${extname(file.originalname)}`)
+          }
+        })
+      }))
+    uploadFile(@UploadedFile() file: Express.Multer.File) {
+        console.log('Uploaded File Successfully!', file);
+
+        return {"originalname":file.originalname};
+    }
+    
     @Get()
     findAll(){
         var str = Math.round(100*Math.random());
