30dfca41492768edfb14850966a16ee63999101a
Prasenjit <prasenjit.aluni@gmail.com>
2024-12-03 04:21:37 +0530

    Many to Many ( Customer & Product )

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "src/product/entities/product.entity";
+import { Column, Entity, ManyToMany, JoinTable, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('tbl_customer')
 export class Customer {
@@ -10,4 +11,21 @@ export class Customer {
 
     @Column()
     mobile: number;
+
+    // Many-to-many relationship with Product
+    @ManyToMany(() => Product, (product) => product.stores)
+    @JoinTable({
+        name: 'tbl_customer_product',  // Custom join table name
+        joinColumn: {
+          name: 'customer_id',   // Custom column name for the Store entity in the join table
+          referencedColumnName: 'id', // Reference column in the Store entity
+        },
+        inverseJoinColumn: {
+          name: 'product_id', // Custom column name for the Product entity in the join table
+          referencedColumnName: 'id', // Reference column in the Product entity
+        },
+    }) // Specifies that this is the owning side of the relationship
+    products: Product[];
+
+
 }
--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,3 +1,4 @@
+import { Customer } from "src/customer/entities/customer.entity";
 import { Productdetail } from "src/productdetail/entities/productdetail.entity";
 import { Store } from "src/store/entities/store.entity";
 import { Entity, Column, ManyToMany, OneToOne, JoinColumn, PrimaryGeneratedColumn } from "typeorm";
@@ -10,14 +11,19 @@ export class Product {
     @Column()
     name: string;
 
-    // Many-to-many relationship with Store
-    @ManyToMany(() => Store, (store) => store.products)
-    stores: Store[];
-
     // One-to-one relationship with ProductDetails
     @OneToOne(() => Productdetail, (productDetails) => productDetails.product)
     @JoinColumn() // Specifies that this is the owning side of the relationship
     details: Productdetail;
+
+
+    // Many-to-many relationship with Store
+    @ManyToMany(() => Store, (store) => store.products)
+    stores: Store[];
+
+    @ManyToMany(() => Customer, (customer) => customer.products)
+    customers: Customer[];
+
 
 
 }
